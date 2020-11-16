d15f39b1f37321b37139770aef7443ce2879a7e7
Artem Leshchev <usethepowerluke@gmail.com>
2020-11-16 22:40:55 +0300

    OpenApi 3 components types are described

--- src/swagger/v3/parameter.ts
+++ src/swagger/v3/parameter.ts
@@ -1,9 +1,9 @@
 import { IOpenAPI3Reference } from './reference';
-import { IOpenAPI3Schema } from './schemas/schema';
+import { IOpenAPI3SimpleSchema } from './schemas/schema';
 
 export interface IOpenAPI3Parameter {
     name: string;
     in: 'query' | 'header' | 'path' | 'cookie';
     required?: boolean;
-    schema?: IOpenAPI3Schema | IOpenAPI3Reference;
+    schema?: IOpenAPI3SimpleSchema | IOpenAPI3Reference;
 }
--- src/swagger/v3/schemas/schema.ts
+++ src/swagger/v3/schemas/schema.ts
@@ -1,14 +1,12 @@
 import { IOpenAPI3BooleanSchema } from './boolean-schema';
 import { IOpenAPI3DateSchema } from './date-schema';
-import { IOpenAPI3EnumSchema } from './enum-schema';
 import { IOpenAPI3GuidSchema } from './guid-schema';
 import { IOpenAPI3NumberSchema } from './number-schema';
 import { IOpenAPI3StringSchema } from './string-schema';
 
-export type IOpenAPI3Schema =
+export type IOpenAPI3SimpleSchema =
     IOpenAPI3StringSchema |
     IOpenAPI3NumberSchema |
     IOpenAPI3GuidSchema |
-    IOpenAPI3EnumSchema |
     IOpenAPI3DateSchema |
     IOpenAPI3BooleanSchema;
