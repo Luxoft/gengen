773fac19835666c1d1db0edc7fe6254e86f68c24
Artem Leshchev <usethepowerluke@gmail.com>
2020-11-17 08:10:03 +0300

    refactor: tests

--- __tests__/swagger/OpenAPIService.spec.ts
+++ __tests__/swagger/OpenAPIService.spec.ts
@@ -3,46 +3,49 @@ import { OpenAPIService } from '../../src/swagger/OpenAPIService';
 describe("OpenAPIService tests", () => {
     describe('ctor ', () => {
         test("old OpenApi version", () => {
-            const spec = '{"version": "1.0.1"}';
-            expect(() => new OpenAPIService(spec)).toThrow('Only OpenApi version 3 supported yet.');
+            const spec = { "version": "1.0.1" };
+            expect(() => new OpenAPIService(JSON.stringify(spec))).toThrow('Only OpenApi version 3 supported yet.');
         });
 
         test("future OpenApi version", () => {
-            const spec = '{"openapi": "4.0.1"}';
-            expect(() => new OpenAPIService(spec)).toThrow('Only OpenApi version 3 supported yet.');
+            const spec = { "openapi": "4.0.1" };
+            expect(() => new OpenAPIService(JSON.stringify(spec))).toThrow('Only OpenApi version 3 supported yet.');
         });
     });
 
     describe('getEndpoints ', () => {
         test("not found", () => {
-            const spec = '{"openapi": "3.0.1" }';
-            const service = new OpenAPIService(spec);
+            const spec = { "openapi": "3.0.1" };
+            const service = new OpenAPIService(JSON.stringify(spec));
             expect(service.getEndpoints()).toEqual([]);
         });
 
         test("sorted", () => {
-            const spec = '{"openapi": "3.0.1", "paths": { "/product/SearchProducts": {}, "/api/v1/Category/AddCategory": {}, "/product/GetProducts": {} } }';
-            const service = new OpenAPIService(spec);
+            const spec = { "openapi": "3.0.1", "paths": { "/product/SearchProducts": {}, "/api/v1/Category/AddCategory": {}, "/product/GetProducts": {} } };
+            const service = new OpenAPIService(JSON.stringify(spec));
             expect(service.getEndpoints()).toEqual(['/api/v1/Category/AddCategory', '/product/GetProducts', '/product/SearchProducts']);
         });
     });
 
     describe('getTagsByEndpoint ', () => {
         test("not found path", () => {
-            const spec = '{"openapi": "3.0.1" }';
-            const service = new OpenAPIService(spec);
+            const spec = { "openapi": "3.0.1" };
+            const service = new OpenAPIService(JSON.stringify(spec));
             expect(service.getTagsByEndpoint("test")).toEqual([]);
         });
 
         test("not found path item", () => {
-            const spec = '{"openapi": "3.0.1", "paths": { "/product/SearchProducts": {} } }';
-            const service = new OpenAPIService(spec);
+            const spec = { "openapi": "3.0.1", "paths": { "/product/SearchProducts": {} } };
+            const service = new OpenAPIService(JSON.stringify(spec));
             expect(service.getTagsByEndpoint("test")).toEqual([]);
         });
 
         test.each(['get', 'post', 'put', 'delete'])('find', (operation) => {
-            const spec = `{"openapi": "3.0.1", "paths": { "test": { "${operation}": { "tags": ["1", "2"] } } } }`;
-            const service = new OpenAPIService(spec);
+            const operationObject: Record<string, { tags: string[] }> = {};
+            operationObject[operation] = { "tags": ["1", "2"] }
+
+            const spec = { "openapi": "3.0.1", "paths": { "test": operationObject } };
+            const service = new OpenAPIService(JSON.stringify(spec));
             expect(service.getTagsByEndpoint("test")).toEqual(["1", "2"]);
         })
     });
