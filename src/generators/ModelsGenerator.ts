5c60998c73248617a9a41447e70ce3b2fdd77588
Artem Leshchev <usethepowerluke@gmail.com>
2020-11-25 00:21:02 +0300

    refactor: Move interface generation to ts-morph

--- src/generators/ModelsGenerator.ts
+++ src/generators/ModelsGenerator.ts
@@ -2,18 +2,16 @@ import { PropertyDeclarationStructure, Scope, StatementStructures, StructureKind
 
 import { IEnumModel } from '../models/EnumModel';
 import { IIdentityModel } from '../models/IdentityModel';
+import { IInterfaceModel } from '../models/InterfaceModel';
 import { IModelsContainer } from '../models/ModelsContainer';
-import { first } from '../utils';
-
-const COMMON_IDENTITY_NAME = 'IdentityDTO';
 
 export class ModelsGenerator {
     public getModelsCodeStructure(models: IModelsContainer): StatementStructures[] {
         return [
             ...this.getImports(),
             ...this.getEnums(models.enums),
-            ...this.getInterfaces(models.identities),
-            ...this.getIdentities(models.identities)
+            ...this.getInterfaces(models.interfaces),
+            ...this.getIdentities(models.identities, models.interfaces)
         ];
     }
 
@@ -41,22 +39,16 @@ export class ModelsGenerator {
         }));
     }
 
-    private getInterfaces(identities: IIdentityModel[]): StatementStructures[] {
-        const result: StatementStructures[] = [];
-        if (identities.length) {
-            const identityModel = first(identities);
-            result.push({
-                kind: StructureKind.Interface,
-                name: this.getInterfaceName(COMMON_IDENTITY_NAME),
-                isExported: true,
-                properties: [{ name: identityModel.property.name, type: identityModel.property.dtoType }]
-            });
-        }
-
-        return result;
+    private getInterfaces(interfaces: IInterfaceModel[]): StatementStructures[] {
+        return interfaces.map((z) => ({
+            kind: StructureKind.Interface,
+            name: z.name,
+            isExported: true,
+            properties: z.properties.map((x) => ({ name: x.name, type: x.dtoType }))
+        }));
     }
 
-    private getIdentities(identities: IIdentityModel[]): StatementStructures[] {
+    private getIdentities(identities: IIdentityModel[], interfaces: IInterfaceModel[]): StatementStructures[] {
         return identities.map((z) => ({
             kind: StructureKind.Class,
             isExported: true,
@@ -67,17 +59,18 @@ export class ModelsGenerator {
                     statements: `this.${z.property.name} = new ${z.property.type}(${z.property.name});`
                 }
             ],
-            properties: [
-                { scope: Scope.Public, name: z.property.name, type: z.property.type, initializer: `${undefined}` },
-                this.getGuardProperty(z.name)
-            ],
+            properties: [{ scope: Scope.Public, name: z.property.name, type: z.property.type }, this.getGuardProperty(z.name)],
             methods: [
                 {
                     scope: Scope.Public,
                     isStatic: true,
                     name: 'toDTO',
                     parameters: [{ name: z.property.name, type: z.property.type }],
-                    returnType: this.getInterfaceName(COMMON_IDENTITY_NAME),
+                    returnType: interfaces.find(
+                        (i) =>
+                            i.properties.length === 1 &&
+                            i.properties.every((x) => x.dtoType === z.property.dtoType && x.name === z.property.name)
+                    )?.name,
                     statements: `return { ${z.property.name}: ${z.property.name}.toString() };`
                 }
             ]
@@ -91,9 +84,5 @@ export class ModelsGenerator {
             name: `_${name[0].toLowerCase()}${name.slice(1)}`,
             type: 'string'
         };
-    }
-
-    private getInterfaceName(name: string): string {
-        return `I${name}`;
     }
 }
