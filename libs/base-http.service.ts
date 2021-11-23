9f421819943be0c138f707bbe6fa2e97ec825d9f
sevaru <sevaru@inbox.ru>
2021-11-23 16:21:04 +0300

    fix: build for angular 13 and strict = true (#100)

--- libs/base-http.service.ts
+++ libs/base-http.service.ts
@@ -1,20 +1,25 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import type { HttpClient, HttpContext, HttpHeaders, HttpParams } from '@angular/common/http';
+import type { Observable } from 'rxjs';
 
 export interface IAngularHttpRequestOptions {
-    headers?: HttpHeaders | { [header: string]: string | string[] };
-    observe?: 'body' | 'response';
-    params?: HttpParams | { [param: string]: string | string[] };
+    headers?: HttpHeaders | {
+        [header: string]: string | string[];
+    };
+    observe?: 'body';
+    context?: HttpContext;
+    params?: HttpParams | {
+        [param: string]: string | number | boolean | ReadonlyArray<string | number | boolean>;
+    };
     reportProgress?: boolean;
-    responseType?: 'json' | 'blob';
+    responseType?: 'json';
     withCredentials?: boolean;
 }
 
 export abstract class BaseHttpService {
-    constructor(private relativePath: string, protected http: HttpClient) {}
+    constructor(private relativePath: string, protected http: HttpClient) { }
 
     protected get<TResult>(url: string, options?: IAngularHttpRequestOptions): Observable<TResult> {
-        return this.http.get<TResult>(this.getPath(url), options as unknown);
+        return this.http.get<TResult>(this.getPath(url), options);
     }
 
     protected post<TResult, TData = {}>(
@@ -22,11 +27,11 @@ export abstract class BaseHttpService {
         data?: TData,
         options?: IAngularHttpRequestOptions
     ): Observable<TResult> {
-        return this.http.post<TResult>(this.getPath(url), data, options as unknown);
+        return this.http.post<TResult>(this.getPath(url), data, options);
     }
 
     protected delete<TResult>(url: string, options?: IAngularHttpRequestOptions): Observable<TResult> {
-        return this.http.delete<TResult>(this.getPath(url), options as unknown);
+        return this.http.delete<TResult>(this.getPath(url), options);
     }
 
     public get path(): string {
